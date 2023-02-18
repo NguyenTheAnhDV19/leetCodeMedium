8c90cd10c06087ae5086b4c523a163ec96ab89f1
BoyAnh <90632032+NguyenTheAnhDV19@users.noreply.github.com>
2023-02-19 00:29:19 +0700

    Update Generate_Parentheses.cpp

--- Generate_Parentheses.cpp
+++ Generate_Parentheses.cpp
@@ -10,25 +10,18 @@ public:
         temp.push_back("()");
         store.push_back(temp);
         temp.clear();
-
         backTrack(1,n);
-
-        for(int i=0;i<store[n-1].size();i++) std::cout<<store[n-1][i]<<std::endl;
-
         return store[n-1];
     }
 };
 
 void Solution::backTrack(int pos, int n)
     {
-        for(int i=0;i<store[0].size();i++)
-        {
-            for(int j=0;j<store[pos-1].size();j++)
-            {
-                temp.push_back(store[0][i]+ store[pos-1][j]);
-                temp.push_back("(" +store[pos-1][j] + ")");
-            }
-        }
+    for(int j=0;j<store[pos-1].size();j++)
+       {
+            temp.push_back(store[0][0]+ store[pos-1][j]);
+            temp.push_back("(" +store[pos-1][j] + ")");
+       }
         store.push_back(temp);
         temp.clear();
         if(pos-1 != n) backTrack(pos+1,n);
@@ -36,7 +29,6 @@ void Solution::backTrack(int pos, int n)
 
 int main(){
     Solution* solution = new Solution;
-    std::vector<int> vect = {-1,2,1,-4};
     solution->generateParenthesis(4);
     return 0;
 }
